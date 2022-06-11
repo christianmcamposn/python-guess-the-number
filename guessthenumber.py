4acf3cfda593234d3c92e6945c7a8381025ec38a
christianmcamposn <84096085+christianmcamposn@users.noreply.github.com>
2022-06-11 13:51:47 -0500

    First version

---
+++ guessthenumber.py
@@ -0,0 +1,32 @@
+"""
+Guess a number game with the user
+"""
+
+import random
+
+def main():
+    """Input the upper and lower limit of a range of numbers
+       and lets the user guess the random number until correct.
+    """
+    lower_limit = int(input("Enter the smaller number of the range: "))
+    upper_limit = int(input("Enter the larger number of the range: "))
+
+    secret_number = random.randint(lower_limit, upper_limit)
+    tries = 0
+
+    while True:
+        tries += 1
+        user_number = int(input("Enter a number to try a guess: "))
+
+        if user_number < secret_number:
+            print("Try with a higher number.")
+        elif user_number > secret_number:
+            print("Try with a lower number.")
+        else:
+            print("You won! It took you {} tries".format(tries) )
+            break
+
+
+if __name__ == "__main__":
+    print(type(__name__))
+    main()
